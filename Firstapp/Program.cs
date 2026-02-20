16cfa61c944a8a06b4ccbc1df29bb48ff2feea3d
Komal <komal@digitalglobal.co.in>
2026-02-20 08:54:04 +0530

    myproject

---
+++ Firstapp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main ()
+    {
+     Console.WriteLine("Custom Password Generator");
+     Console.Write("Enter your name: ");
+     string name = Console.ReadLine();
+
+     Console.Write("Enter your phone number: ");
+     string phone = Console.ReadLine();
+
+     string last4= phone.Length >=4 ? phone.Substring(phone.Length -4 ):phone;
+
+     string Password = name + last4;
+
+     Console.WriteLine("your generated password is : "+ Password);
+     Console.ReadKey();
+    }
+}
